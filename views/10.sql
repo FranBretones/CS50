a565b0f81ec000faaeb63a78dea0285b45ca2324
FranciscoBretones <129505823+FranBretones@users.noreply.github.com>
2023-12-04 17:58:57 +0100

    Delete views directory

--- views/10.sql
+++
@@ -1,3 +0,0 @@
-SELECT english_title AS "Top3 Highest Entropy" FROM views
-ORDER BY entropy DESC
-LIMIT 3;
--- views/5.sql
+++
@@ -1,2 +0,0 @@
-SELECT MAX (contrast) AS Maximun_contrast FROM views
-WHERE artist = "Hokusai";
